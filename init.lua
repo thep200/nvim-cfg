f9add3f4de9a7a75bdbcbf0707ae95276c8437ad
thep200 <thep200.work@gmail.com>
2026-05-28 08:35:02 +0700

    fix patch

--- init.lua
+++ init.lua
@@ -7,9 +7,9 @@ require("core.options")
 require("core.keymaps")
 require("core.autocmds")
 
+-- Compat: ft_to_lang bị bỏ ở Neovim 0.11, redirect sang get_lang
 if vim.treesitter.language and not vim.treesitter.language.ft_to_lang then
     vim.treesitter.language.ft_to_lang = vim.treesitter.language.get_lang
-        or function() return nil end
 end
 
 require("plugins")
